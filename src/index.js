f1d7100fabbc5b8ce942bc99bbddd1d01ca356b6
Ryan Hefner <hi@ryanhefner.com>
2021-01-27 23:27:07 -0500

    Update JSDoc declaration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 /**
+ * nextProps - Compose props providers for `getServerSideProps` and `getStaticProps` NextJS methods.
+ *
  * @param {{ initialProps: any }}
  * @return {(handlers: ((props: any, next: (((props: any) => Promise<GetServerSidePropsResult<{ props: any } | { notFound: boolean } | { redirect: Redirect }>>), ...args: any[]) => Promise<GetServerSidePropsResults<{ props: any } | { notFound: boolean } | { redirect: Redirect }>)[]) => (...args: any[]) => Promise<GetServerSidePropsResult<{ props: any } | { notFound: boolean } | { redirect: Redirect }>>}
  */
--- types/index.d.ts
+++ types/index.d.ts
@@ -1,5 +1,7 @@
 export default nextProps;
 /**
+ * nextProps - Compose props providers for `getServerSideProps` and `getStaticProps` NextJS methods.
+ *
  * @param {{ initialProps: any }}
  * @return {(handlers: ((props: any, next: (((props: any) => Promise<GetServerSidePropsResult<{ props: any } | { notFound: boolean } | { redirect: Redirect }>>), ...args: any[]) => Promise<GetServerSidePropsResults<{ props: any } | { notFound: boolean } | { redirect: Redirect }>)[]) => (...args: any[]) => Promise<GetServerSidePropsResult<{ props: any } | { notFound: boolean } | { redirect: Redirect }>>}
  */
