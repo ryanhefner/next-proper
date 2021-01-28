790ba04748cf48547c5b814a806bf942e46c612a
Ryan Hefner <hi@ryanhefner.com>
2021-01-27 22:46:00 -0500

    Update TypeScript declarations

--- types/index.d.ts
+++ types/index.d.ts
@@ -1,16 +1,12 @@
+import type { Redirect } from 'next'
+
 interface Config {
   initialState?: any;
 }
 
-interface Props {
-  notFound?: boolean;
-  props?: any;
-  redirect?: any;
-}
+declare async function init(ctx: any): Promise<{ props: any } | { notFound: boolean } | { redirect: Redirect }>;
 
-declare function init(ctx: any): Props;
-
-declare function handler(props: Props, next, ...args: any[]): Props;
+declare function handler(props: Props, next, ...args: any[]): Promise<GetServerSidePropsResult<any>>;
 
 declare function nextHandlers(handlers: handler[]): init;
 
