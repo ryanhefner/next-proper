5ce1b52e80c40025c514db2305ee7d9e0bf8c52c
Ryan Hefner <hi@ryanhefner.com>
2021-02-05 15:39:14 -0500

    Update TypeScript declarations

--- types/index.d.ts
+++ types/index.d.ts
@@ -2,9 +2,8 @@ export default nextProps;
 /**
  * nextProps - Compose props providers for `getServerSideProps` and `getStaticProps` NextJS methods.
  *
- * @param {{ initialProps: any }}
- * @return {(handlers: ((props: any, next: (((props: any) => Promise<GetServerSidePropsResult<{ props: any } | { notFound: boolean } | { redirect: Redirect }>>), ...args: any[]) => Promise<GetServerSidePropsResults<{ props: any } | { notFound: boolean } | { redirect: Redirect }>)[]) => (...args: any[]) => Promise<GetServerSidePropsResult<{ props: any } | { notFound: boolean } | { redirect: Redirect }>>}
+ * @param {Object=} config
+ * @param {Object=} config.initialProps
+ * @return {(handlers: ((props: any, next: (((props: any) => Promise<GetServerSidePropsResult<any>>), ...args: any[]) => Promise<GetServerSidePropsResults<any>>}
  */
-declare function nextProps({ initialProps }: {
-    initialProps: any;
-}): (handlers: ((props: any, next: (props: any) => Promise<any>, ...args: any[]) => Promise<any>)[]) => (...args: any[]) => Promise<any>;
+declare function nextProps(config?: any | undefined): (handlers: (props: any, next: (props: any) => Promise<any>, ...args: any[]) => Promise<any>) => any;
